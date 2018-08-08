f5331f1876e067493714b207ecaa64e02a37858f
Aaron Magnaye <avmagnaye@up.edu.ph>
2018-08-08 14:59:35 +0800

    Added default instances of Ingredient

--- src/database/Ifpms.sql
+++ src/database/Ifpms.sql
@@ -3,11 +3,18 @@
 drop database if exists ifpms;
 create database ifpms;
 
+use ifpms;
+
 create table Ingredient(
-    ingredient_id int not null AUTO_INCREMENT,
+    ingredient_id int AUTO_INCREMENT,
+    ingredient_name varchar(45),
     ingredient_type varchar(45),
-    quantity int not null,
     cost int not null,
-    unit varchar(45)
+    unit varchar(45),
+    PRIMARY KEY(ingredient_id)
 );
+
+-- create default instances of Ingredient
+insert into Ingredient values(null, 'bawang', 'gulay', 2, 'piece');
+insert into Ingredient values(null, 'sibuyas', 'gulay', 2, 'piece');
 
