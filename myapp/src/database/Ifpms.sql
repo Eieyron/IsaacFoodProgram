25858bb94500482cb6cbf840e0fde2f38f579592
Aaron Magnaye <avmagnaye@up.edu.ph>
2018-08-23 08:32:52 +0800

    Added database information for MEAL Class

--- myapp/src/database/Ifpms.sql
+++ myapp/src/database/Ifpms.sql
@@ -60,3 +60,61 @@ call addIngredient( 'bawang', 'vegetables', 2, 'piece');
 call addIngredient( 'sibuyas', 'vegetables', 2, 'piece');
 
 /*** END OF INGREDIENT RELATED CODE ***/
+
+/*** Start of MEAL Related Code ***/
+
+create table MEAL (
+    meal_id int AUTO_INCREMENT,
+    meal_name varchar(45),
+    meal_type varchar(45),
+    meal_time varchar(45),
+    recipe varchar(256),
+    PRIMARY KEY(meal_id)
+);
+
+-- procedures for Ingredient
+
+drop procedure if exists viewAllMeals;
+drop procedure if exists viewMealByID;
+drop procedure if exists addMeal;
+drop procedure if exists deleteMealByID;
+
+delimiter //
+
+    create PROCEDURE viewAllMeals()
+        BEGIN
+            select * from Meal;
+        end;
+    //
+
+    create PROCEDURE viewMealByID( meal_id_view int )
+        begin  
+            select * from Meal where meal_id = meal_id_view;
+        end;
+    //
+
+    create procedure addMeal (name varchar(45),
+                              type varchar(45),
+                              time VARCHAR(45),
+                              recipe VARCHAR(255))
+        BEGIN
+            insert into MEAL values(null, name, type, time, recipe);
+        end;
+    //
+
+    create procedure deleteMealByID( meal_id_delete int )
+        BEGIN  
+            delete from meal where meal_id = meal_id_delete;
+        end;
+    //
+
+delimiter ;
+
+-- create default instances of Meal
+
+call addMeal( 'adobo', 'meat', 'dinner', 'do everything');
+call addMeal( 'tuna cabbage', 'vegetable', 'breakfast', 'do everything');
+call addMeal( 'pininyahang manok', 'meat', 'dinner', 'do everything');
+call addMeal( 'hotdog + egg', 'meat', 'breakfast', 'do everything');
+
+/*** End of MEAL RELATED CODE ***/
