cb4aa4c65f7145860b78819e33e1e8faf8b585e9
Aaron Magnaye <avmagnaye@up.edu.ph>
2018-08-23 08:58:13 +0800

    finished CRUD for Ingredient.js

--- myapp/routes/Ingredient.js
+++ myapp/routes/Ingredient.js
@@ -4,7 +4,8 @@ var express = require('express');
 var router = express.Router();
 var ingredient = require('../models/Ingredient');
 
-router.get('/:id?', (req, res, next)=>{
+router.get('/:id', (req, res, next)=>{
+
     if(req.params.id) {
         ingredient.viewIngredientByID(req.params.id, (err, rows)=>{
             if(err){
@@ -22,6 +23,24 @@ router.get('/:id?', (req, res, next)=>{
             }
         });
     }
+});
+
+router.post('/addingredient/:name/:type/:cost/:unit', (req, res, next)=>{
+
+    ingredient.addIngredient(req.params, (err, rows)=>{
+        if(err)res.json(err);
+        else res.json(rows);
+    });
+
+});
+
+router.delete('/delete/:id', (req,res,next)=>{
+
+    ingredient.deleteIngredientByID(req.params.id, (err, rows)=>{
+        if(err)res.json(err);
+        else res.json(rows);
+    });
+
 });
 
 module.exports = router;
