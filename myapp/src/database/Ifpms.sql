adcd6763853b0434a3e8dd809403545bad415cf1
Aaron Magnaye <avmagnaye@up.edu.ph>
2018-08-24 12:49:28 +0800

    added default mealingredient instances

--- myapp/src/database/Ifpms.sql
+++ myapp/src/database/Ifpms.sql
@@ -192,5 +192,6 @@ delimiter ;
 call addMealIngredient(1,1,1);
 call addMealIngredient(1,2,1);
 call addMealIngredient(2,2,3);
+call addMealIngredient(3,1,1);
 
 /*** End of MealIngredient Connector Table ***/
