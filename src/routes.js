665913d797d589259412960608029f9123244e80
dom <domlapitan@gmail.com>
2019-01-16 23:12:11 +0800

    Add Ingredient Controller

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,22 @@
 const IndexController = require('./controllers/IndexController')
+const IngredientController = require('./controllers/IngredientController')
 
 module.exports = (app) => {
 
   // Home Page
   app.get('/',
     IndexController.home)
+
+  // INGREDIENT-RELATED
+  app.get('/view-ingredient/:id',
+    IngredientController.viewIngredient)
+  app.get('/view-ingredients',
+    IngredientController.viewAllIngredients)
+  app.post('/add-ingredient',
+    IngredientController.addIngredient)
+  app.put('/update-ingredient/:id',
+    IngredientController.updateIngredient)
+  app.delete('/delete-ingredient/:id',
+    IngredientController.deleteIngredient)
+
 }
