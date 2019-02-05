2b20cf18a07843c339f0571f88f62f09ea3d763f
dom <domlapitan@gmail.com>
2019-02-05 19:06:27 +0800

    Edit temp database

--- src/foodprogram.sql
+++ src/foodprogram.sql
@@ -4,17 +4,17 @@ CREATE DATABASE foodprogram;
 USE foodprogram;
 
 CREATE TABLE ingredients (
-  ingredient_id int AUTO_INCREMENT PRIMARY KEY,
-  ingredient_name varchar(256),
-  ingredient_type varchar(45),
+  ingredient_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  ingredient_name varchar(256) NOT NULL,
+  ingredient_type varchar(45) NOT NULL, 
   cost float NOT NULL,
-  unit varchar(45)
+  unit varchar(45) NOT NULL
 );
 
 CREATE TABLE meals (
-    meal_id int AUTO_INCREMENT PRIMARY KEY,
-    meal_name varchar(45),
-    meal_type varchar(45),
-    meal_time varchar(45),
-    recipe varchar(256),
+    meal_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    meal_name varchar(45) NOT NULL,
+    meal_type varchar(45) NOT NULL,
+    meal_time varchar(45) NOT NULL,
+    recipe TEXT NOT NULL
 );
